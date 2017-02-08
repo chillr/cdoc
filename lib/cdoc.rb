fbaf5359995ce6834b1b34d50d42bc2544fe0383
Deepak Kumar <deepakkumarnd@gmail.com>
2017-02-08 18:33:38 +0530

    Generate bootstrap based UI for the documentation

--- lib/cdoc.rb
+++ lib/cdoc.rb
@@ -6,6 +6,12 @@ require 'json'
 
 module Cdoc
 
+  module Helpers
+    def to_id(str)
+      str.gsub(/\W/, '').downcase
+    end
+  end
+
   class DocRenderer < Redcarpet::Render::HTML
     def block_code(code, lang='text')
       lang = lang && lang.split.first || "text"
@@ -14,22 +20,43 @@ module Cdoc
   end
 
   class DocString
+
+    include Helpers
+
     def initialize
       @docstring = ''
+      @content = []
     end
 
     def title(title)
       @title = title
-      @docstring << "\n" + "# #{title}" + "\n"
     end
 
     def section(str)
-      @docstring << "\n" + "## #{str}" + "\n"
+      template = "<p id='%{id}'><h3>%{str}</h3></p>"
+      @content <<  template % { id: to_id(str), str: str }
+    end
+
+    def sidebar(sidebar)
+      @sidebar = sidebar
     end
 
     def subsection(str)
-      lines = str.split("\n")
+
+      template = %q(
+      <div class="panel panel-default" id="accounts">
+        <div class="panel-heading">
+          <h3 class="panel-title">%{section_header}</h3>
+        </div>
+        <div class="panel-body">
+          %{section_body}
+        </div>
+      </div>)
+
+          lines = str.split("\n")
       index = 0
+      section_header = ''
+      sub_section = []
 
       loop do
         line = lines[index]
@@ -51,38 +78,47 @@ module Cdoc
               begin
                 json = JSON.parse(code_str)
                 code_str = JSON.pretty_generate(json)
-                block = ["\n", '```json', code_str, '```'].join("\n")
+                block = ['<pre><code>', code_str, '</code></pre>'].join('')
               rescue JSON::ParserError => e
-                puts e.message
-                block = ["\n", '```', code_str, '```'].join("\n")
+                # puts e.message
+                block = ['<pre><code>', code_str, '</code></pre>'].join("\n")
               end
 
               break
             else
-              code_block << line.sub('  ', '')
+              code_block << line.sub('  ', '') unless line.strip.empty?
               index = index + 1
             end
           end
         else
-          block = line
+          block = [line, '<br/>'].join('')
         end
 
-        @docstring << block
+        if index == 0
+          section_header = block
+        else
+          sub_section << block
+        end
+
         index = index + 1
       end
 
-      @docstring << "\n"
+      @content << template % { section_header: section_header, section_body: sub_section.join("\n")}
     end
 
     def tagged_line(line, tag)
-      t   = line.sub(tag, '').strip
-      t_l = "\n" + tag.sub('@','').capitalize
-
-      if !t.empty?
-        t_l = t_l + " **#{t.strip}**\n\n"
+      if ['url', 'endpoint', 'api'].include?(tag.downcase)
+        # set the title of the section
       end
 
-      t_l
+      t   = line.sub(tag, '').strip
+      t_l = ['<small>', tag.sub('@', '').capitalize, '</small>']
+
+      if !t.empty?
+        t_l = t_l + ['<strong>', t.strip, '</strong>']
+      end
+
+      ['<p>', t_l, '</p>'].flatten.join("\n")
     end
 
     def finish
@@ -97,13 +133,17 @@ module Cdoc
       f.close
     end
 
+    def content
+      @content.flatten.join('')
+    end
+
     def render_as_html
-      layout_file = File.join(File.dirname(__FILE__), 'layouts/default.html')
+      layout_file = File.join(File.dirname(__FILE__), 'layouts/bootstrap.html')
       layout = File.read(layout_file)
       f = File.open('doc/index.html', 'w+')
       renderer = DocRenderer.new
       markdown = Redcarpet::Markdown.new(renderer, fenced_code_blocks: true)
-      html = layout % { title: @title, content: markdown.render(@docstring)}
+      html = layout % { title: @title, sidebar: @sidebar, content: content }
       f.write(html)
       f.close
 
@@ -116,6 +156,8 @@ module Cdoc
 
   class CDoc
 
+    include Helpers
+
     attr_accessor :files, :docstring
 
     def initialize
@@ -124,10 +166,24 @@ module Cdoc
       @doc = DocString.new
     end
 
+    def generate_sidebar(keys)
+      tmpl_item = '<a href="#%{id}" class="list-group-item">%{item}</a>'
+
+      items = keys.map do |key|
+        tmpl_item % { id: to_id(key), item: key.capitalize }
+      end
+
+      ['<div class="list-group">', items, '</div>'].flatten.join("\n")
+    end
+
     def generate
       @doc.title('Chillr API Documentaion')
 
       @file_groups = files.group_by { |f| File.basename(f, '_controller.rb') }
+
+      sidebar = generate_sidebar(@file_groups.keys)
+
+      @doc.sidebar(sidebar)
 
       @file_groups.each do |group, files|
         @doc.section(group.capitalize)
